9e697e975b9c929e9b2893672f9ba954d3197cea
Alejandro Vázquez Carrión <123106711+ElTinii@users.noreply.github.com>
2023-10-20 20:26:59 +0200

    arreglem algunes coses

--- Model/iniciar_model.php
+++ Model/iniciar_model.php
@@ -1,5 +1,26 @@
 <?php
-$name = "pt03_alex_vazquez";
-$dbuser = "root";
-$connexio = new PDO($name, $dbuser,'');
+    $username = filtrado($_POST["username"]);
+    $password = filtrado($_POST["password"]);
+    $name = "pt03_alex_vazquez";
+    $dbuser = "root";
+    $connexio = new PDO("mysql:host=localhost;dbname=$name", $dbuser,'');
+
+    $consulta = $connexio->prepare('SELECT password FROM usuaris WHERE username = ?');
+    $consulta->bindParam(1, $username);
+    $consulta->execute();
+    $resultado = $consulta->fetch();
+    $desc_passw = password_verify($password,$resultado);
+
+    if ($resultado) {
+        if(password_verify($password,$resultado)){
+            start_session();
+            $_SESSION['username'] = $username;
+            include_once "../usuari_vista.php";
+        }else{
+            $errors = "La contrasenya no es correcta";
+        }
+        
+    } else {
+        $errors = "No hemos encontrado el usuario";
+    }
 ?>
--- Model/register_model.php
+++ Model/register_model.php
@@ -5,11 +5,13 @@ try {
     $name = "pt03_alex_vazquez";
     $dbuser = "root";
     $connexio = new PDO("mysql:host=localhost;dbname=$name", $dbuser,'');
+    
     $consulta = $connexio->prepare('SELECT * FROM usuaris WHERE username = ?');
     $consulta->bindParam(1, $username);
     $consulta->execute();
     $resultado = $consulta->fetch();
     $encr_pass = password_hash($password,PASSWORD_DEFAULT);
+
     if ($resultado) {
         $errors = "Este username ya esta inserido";
     } else {
@@ -17,10 +19,9 @@ try {
         $stmt->bindParam(1, $username);
         $stmt->bindParam(2, $encr_pass);
         if ($stmt->execute()) {
-            $errors = "Usuari inserit correctament";
             include_once "../Vista/usuari_vista.php";
         } else {
-            $errors = "Error al inserir l'usuari";
+            $errors = "Error al inserir el usuario";
         }
     }
 } catch (PDOException $e) {
